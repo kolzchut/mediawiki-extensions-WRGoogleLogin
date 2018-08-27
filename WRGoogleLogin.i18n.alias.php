e1c0909abe0fcc74046bf5e301acf8a2094c50b3
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2018-08-27 14:36:34 +0300

    Add aliases for the special pages

---
+++ WRGoogleLogin.i18n.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for WRGoogleLogin
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English
+ * @author FreedomFighterSparrow
+ */
+$specialPageAliases['en'] = [
+	'GoogleSignin' => [ 'GoogleSignin', 'Google Signin', 'Google Login' ],
+	'GoogleReturn' => [ 'GoogleReturn', 'Google Return' ]
+];
